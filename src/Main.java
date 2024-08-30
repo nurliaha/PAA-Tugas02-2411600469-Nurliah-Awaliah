d23f01dc459f86b8a76c42b39423146ea40bfe6b
nurliaha <nurliaha@gmail.com>
2024-08-30 20:45:58 +0700

    feature: add verbose answer and breakdown

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 //        Seseorang berangkat pukul  08 : 52 : 45
@@ -13,20 +11,34 @@ public class Main {
         int second = 45;
         int durationInSeconds = 5000;
 
+        System.out.println("========== Question 1 ==========");
+        System.out.println("Departure Time:");
+        System.out.println("Hour: " + hour);
+        System.out.println("Minute: " + minute);
+        System.out.println("Second: " + second);
+        System.out.println("Flight Duration in seconds: " + durationInSeconds);
+        System.out.println("========== BREAKDOWN ==========");
         int convertedHourToSeconds = hour * 3600;
         int convertedMinuteToSecond = minute * 60;
         int totalInSecond = convertedMinuteToSecond + convertedHourToSeconds + second + durationInSeconds;
+        System.out.println("Convert Hour to Second: 8 * 3600 = " + convertedHourToSeconds);
+        System.out.println("Convert Minute to Second: 52 * 60 = " + convertedHourToSeconds);
+        System.out.println("Second: " + second);
+        System.out.println("Flight Duration in seconds: " + durationInSeconds  + " second");
 
         int arrivedHour = totalInSecond / 3600; // jam
         int arrivedMinute = (totalInSecond % 3600) / 60;
         int arrivedSecond = (totalInSecond % 3600) % 60;
+        System.out.println("Answer 1st Question, Hour + Minute + Second + 5000 second = " + totalInSecond + " seconds");
+        System.out.println("Arrived at the destination with Converted Second To Hour, Minute and Second");
+        System.out.println("========== ANSWER ==========");
+        System.out.println("Arrived Time:");
+        System.out.println("Hour: " + arrivedHour);
+        System.out.println("Minute: " + arrivedMinute);
+        System.out.println("Second: " + arrivedSecond);
+        System.out.println("============================");
 
-        System.out.println("Jam : " + arrivedHour);
-        System.out.println("Menit : " + arrivedMinute);
-        System.out.println("Detik : " + arrivedSecond);
-
-
-//        Seseorang berangkat pukul  08 : 52 : 45
+//  2.      Seseorang berangkat pukul  08 : 52 : 45
 //        ( pukul 08 lewat 52 menit 45 detik ) ,
 //        dan tiba ditujuan pukul : 12 : 15 : 10.
 //        Susun program untuk menghitung dan mencetak berapa jam, berapa menit dan berapa detik waktu yang dia habiskan dalam perjalanan.
@@ -38,44 +50,45 @@ public class Main {
         int arrivedMinutes = 15;
         int arrivedSeconds = 10;
 
-        int departHourstoSeconds = departureHour * 3600;
-        int departMinutetoSeconds = departureMinute * 60;
-        int totalSecondsDepart = departHourstoSeconds + departMinutetoSeconds + departureSecond;
+        System.out.println("========== Question 2 ==========");
+        System.out.println("Departure Time:");
+        System.out.println("Hour: " + hour);
+        System.out.println("Minute: " + minute);
+        System.out.println("Second: " + second);
+        System.out.println("Arrived Time:");
+        System.out.println("Hour: " + hour);
+        System.out.println("Minute: " + minute);
+        System.out.println("Second: " + second);
+        System.out.println("========== BREAKDOWN ==========");
+
+        int departHoursToSeconds = departureHour * 3600;
+        int departMinuteToSeconds = departureMinute * 60;
+        int totalSecondsDepart = departHoursToSeconds + departMinuteToSeconds + departureSecond;
+
+        System.out.println("Convert Departure Hour to Second: 8 * 3600 = " + convertedHourToSeconds);
+        System.out.println("Convert Departure Minute to Second: 52 * 60 = " + convertedHourToSeconds);
+        System.out.println("Departure Second: " + second);
+        System.out.println("Total Departure in Second: " + totalSecondsDepart + " seconds");
 
         int arrivedHourstoSeconds = arrivedHours * 3600;
-        int arrivedMinutestoSeconds = arrivedMinutes * 60 ;
+        int arrivedMinutestoSeconds = arrivedMinutes * 60;
         int totalSecondsArrived = arrivedHourstoSeconds + arrivedMinutestoSeconds + arrivedSeconds;
 
-        int kurangSecondDeptArr = totalSecondsArrived - totalSecondsDepart;
-        int convertedSecondHour = kurangSecondDeptArr  / 3600; // jam
-        int convertedSecondMinute = (kurangSecondDeptArr % 3600) / 60;
-        int leftSecond = (kurangSecondDeptArr % 3600) % 60;
+        System.out.println("Convert Arrived Hour to Second: 12 * 3600 = " + convertedHourToSeconds);
+        System.out.println("Convert Arrived Minute to Second: 15 * 60 = " + convertedHourToSeconds);
+        System.out.println("Departure Second: " + arrivedSeconds);
+        System.out.println("Total Arrived in Second: " + totalSecondsDepart + " seconds");
 
+        int diffSecondDeptArr = totalSecondsArrived - totalSecondsDepart;
+        int diffHourDeptArr = diffSecondDeptArr  / 3600; // jam
+        int diffMinutesDeptArr = (diffSecondDeptArr % 3600) / 60;
+        int leftSecond = (diffSecondDeptArr % 3600) % 60;
 
-
-
-
-//        int resultHours = arrivedHours - departureHour;
-//        int resultMinutes = arrivedMinutes - departureMinute;
-//        int resultSeconds = arrivedSeconds - departureSecond;
-
-//        int resultHourstoSeconds = resultHours * 3600;
-//        int resultMinutetoSeconds = resultMinutes * 60;
-//        int totalSeconds = resultHourstoSeconds + resultMinutetoSeconds + resultSeconds;
-
-        System.out.println("tes");
+        System.out.println("========== ANSWER ==========");
+        System.out.println("Flight Duration in Second: Total Arrived in Second - Total Depart in Second = " + diffSecondDeptArr);
+        System.out.println("Hour: diffSecondDeptArr div 3600 = " + diffHourDeptArr);
+        System.out.println("Minute: (diffHourDeptArr mod 3600) div 60 = " + diffMinutesDeptArr);
+        System.out.println("Second: (diffSecondDeptArr % 3600) % 60 = " + leftSecond);
+        System.out.println("============================");
     }
-//
-//    public int convertHourToSecond(int hours){
-//        return hours * 3600;
-//    }
-//
-//    public int convertMinuteToSecond(int minute){
-//        return minute * 60;
-//    }
-//
-//    public int convertSecondToHours(int minute){
-//        return minute * 60;
-//    }
-
 }
